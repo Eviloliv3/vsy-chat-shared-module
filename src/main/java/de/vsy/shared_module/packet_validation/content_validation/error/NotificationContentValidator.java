444f5dab04e0d44c0ed2ed93274d9ec94d458cc2
Frederic Heath <Frederic.Heath@mail.de>
2023-01-23 15:15:33 +0100

    Renamed ErrorContentValidator to NotificationContentValidator.

--- src/main/java/de/vsy/shared_module/packet_validation/content_validation/error/NotificationContentValidator.java
+++ src/main/java/de/vsy/shared_module/packet_validation/content_validation/error/NotificationContentValidator.java
@@ -3,26 +3,26 @@ package de.vsy.shared_module.packet_validation.content_validation.error;
 import de.vsy.shared_module.packet_exception.PacketValidationException;
 import de.vsy.shared_module.packet_validation.content_validation.BasePacketContentValidator;
 import de.vsy.shared_transmission.packet.content.PacketContent;
-import de.vsy.shared_transmission.packet.content.notification.ErrorDTO;
+import de.vsy.shared_transmission.packet.content.notification.SimpleInformationDTO;
 
 import java.util.ArrayList;
 
 import static de.vsy.shared_module.data_element_validation.StringCheck.checkString;
 
-public class ErrorContentValidator extends BasePacketContentValidator<ErrorDTO> {
+public class NotificationContentValidator extends BasePacketContentValidator<SimpleInformationDTO> {
 
     private static final String STANDARD_VALIDATION_MESSAGE = "Invalid notification content. ";
 
-    public ErrorContentValidator() {
+    public NotificationContentValidator() {
         super(STANDARD_VALIDATION_MESSAGE);
     }
 
     @Override
-    public ErrorDTO castAndValidateContent(PacketContent inputContent)
+    public SimpleInformationDTO castAndValidateContent(PacketContent inputContent)
             throws PacketValidationException {
-        final var errorContent = super.castContent(ErrorDTO.class, inputContent);
+        final var notificationContent = super.castContent(SimpleInformationDTO.class, inputContent);
         final var errorStrings = new ArrayList<String>();
-        final var errorMessage = errorContent.getInformationString();
+        final var errorMessage = notificationContent.getInformationString();
         final var checkString = checkString(errorMessage);
 
         checkString.ifPresent(errorStrings::add);
@@ -30,6 +30,6 @@ public class ErrorContentValidator extends BasePacketContentValidator<ErrorDTO> 
         if (!errorStrings.isEmpty()) {
             throw new PacketValidationException(super.createErrorMessage(errorStrings));
         }
-        return errorContent;
+        return notificationContent;
     }
 }
